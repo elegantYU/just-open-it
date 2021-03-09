75b00dc3c8239848fde5e0318511dd0c217004ba
elegantYU <845384559@qq.com>
2021-03-09 11:00:52 +0800

    fix: wx新增规则

--- src/injectScripts/weixin/index.ts
+++ src/injectScripts/weixin/index.ts
@@ -2,7 +2,8 @@
 export default () => {
 	const { href } = window.location;
 	const reg = /http(s|):\/\/weixin110\.qq\.com\/cgi-bin\/mmspamsupport-bin\/newredirectconfirmcgi\?main_type=5/;
-	const target = document.querySelector('.weui-msg__desc')?.textContent;
+	const selectors = ['.ui-ellpisis-content', '.weui-msg__desc'];
+	const target = selectors.map((s) => document.querySelector(s)?.textContent).find((_) => _);
 
 	if (reg.test(href) && target) {
 		window.location.href = target;
