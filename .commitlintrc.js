ab68d51857b87ccda8a1fda9e0b00a98ad586aee
elegantYU <845384559@qq.com>
2021-03-29 10:16:39 +0800

    version: 1.0.6

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -19,7 +19,7 @@ module.exports = {
 		'type-enum': [
 			2,
 			'always',
-			['build', 'ci', 'chore', 'feat', 'docs', 'fix', 'perf', 'refactor', 'style', 'notes', 'wip'],
+			['build', 'ci', 'chore', 'feat', 'docs', 'fix', 'perf', 'refactor', 'style', 'notes', 'wip', 'version'],
 		],
 	},
 };
